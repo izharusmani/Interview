c69639604f2ca0a4f790b07ae9b9c5588f39c436
unknown <izharusmani786@gmail.com>
2021-06-20 16:27:51 +0530

    Update Index Fiel with About Sectio

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
 				<h1>Innovative Idea</h1>
 				<p>Innovative idea we are implementing</p>
 				<img src="/wp-content/themes/ams/images/banner1.jpg" alt="Banner 1">
-				<?php echo "pizza";
-				echo "Burger"?>
 			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about_cont_spec">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">Content will come</div>
+			<div class="col-md-4">Image will come</div>
 		</div>
 	</div>
 </section>
