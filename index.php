2565f0bb5f29c4bf9823294a6838760128e2f50a
unknown <izharusmani786@gmail.com>
2021-06-20 15:26:32 +0530

    second version of index file

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php 
-echo 'We have setup the git using command and now we are going to learn about the commin and revert and add code and push code and pull request';
+<?php get_header() ?>
+
+<section class="banner_section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Innovative Idea</h1>
+				<p>Innovative idea we are implementing</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer() ?>
