782e0671c3a2df7c811288ea145256115c1f7cfc
unknown <izharusmani786@gmail.com>
2021-06-20 16:04:55 +0530

    Update Index file

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 				<h1>Innovative Idea</h1>
 				<p>Innovative idea we are implementing</p>
 				<img src="/wp-content/themes/ams/images/banner1.jpg" alt="Banner 1">
+				<?php echo "pizza";
+				echo "Burger"?>
 			</div>
 		</div>
 	</div>
